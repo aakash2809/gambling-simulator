fc8345a3af401336adfbafb361aeb5ea4e22080e
aakash2809 <aakashrajak289@gmail.com>
2020-12-15 10:48:32 +0530

    Added: total amount win or loss in 20 days

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -1,5 +1,7 @@
 var stake = 100;
 const BET_AMOUNT = 1;
+var fiftyPercentageLoss = stake - (stake / 100) * 50;
+var fiftyPercentageWin = stake + (stake / 100) * 50;
 
 //check for win or loss
 checkWinOrLoss = () => {
@@ -7,16 +9,32 @@ checkWinOrLoss = () => {
         console.log("Win") : console.log("Loss");
 }
 
-//This mthod for resign for the day if 50% loss or 50% win
+//This mthod for resign for the day if 50% loss or 50% win and re
 resignDay = () => {
     let cash = stake;
-    while (cash < 150 && cash > 50) {
+    while (cash < fiftyPercentageWin && cash > fiftyPercentageWin) {
         ((Math.floor(Math.random() * 10) % 2) == BET_AMOUNT) ?
             (cash = cash + 1) : (cash = cash - 1);
     }
-    console.log(cash);
+    return cash;
 }
 
-console.log("Welcome to Gambling Simulator");
-checkWinOrLoss();
-resignDay();
+//This method is for play and update win and loss money day wise
+gamblingToatalAmountUpdate = () => {
+    var day = 1;
+    var winOrLossMoney = 0;
+    while (day <= 20) {
+        if (resignDay() == fiftyPercentageWin) {
+            winOrLossMoney = fiftyPercentageWin - stake;
+            console.log("amount win on day " + day + " is " + winOrLossMoney);
+        }
+        else {
+            winOrLossMoney = stake - fiftyPercentageLoss;
+            console.log("amount loss on day " + day + " is " + winOrLossMoney);
+        }
+        day = day + 1;
+    }
+}
+
+console.log("\nWelcome to Gambling Simulator\n");
+gamblingToatalAmountUpdate();
