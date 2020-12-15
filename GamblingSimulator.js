dd86c6600c7a2fa1ae2d4cf34e27dc380d8934e9
aakash2809 <aakashrajak289@gmail.com>
2020-12-15 09:37:02 +0530

    Added : Win or Loss

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -1,4 +1,10 @@
 var stake = 100;
 const BET_AMOUNT = 1;
 
+checkWinOrLoss = () => {
+    ((Math.floor(Math.random() * 10) % 2) == BET_AMOUNT) ?
+        console.log("Win") : console.log("Loss");
+}
+
 console.log("Welcome to Gambling Simulator");
+checkWinOrLoss();
