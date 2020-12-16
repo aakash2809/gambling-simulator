37806bfab01df178976f73a087551582ea79c02f
aakash2809 <aakashrajak289@gmail.com>
2020-12-17 04:33:56 +0530

    Added: Find maximum luckiest and maximum unluckiest day

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -2,18 +2,32 @@ var stake = 100;
 const BET_AMOUNT = 1;
 var fiftyPercentageLoss = stake - (stake / 100) * 50;
 var fiftyPercentageWin = stake + (stake / 100) * 50;
-var NumberOfMonthsPerMonth = {
+var maximumWinMonth;
+var maxiMumLossMonth;
+var NumberOfDaysPerMonth = {
     "Jan": 31, "Feb": 28, "Mar": 31, "Apr": 30, "May": 31, "Jun": 30,
     "jul": 31, "Aug": 31, "Sep": 30, "Oct": 31, "Nov": 30, "Dec": 31
 };
+var dayNameMap = {
+    "1": "mon", "2": "Tue", "3": "Wed", "4": "Thu",
+    "5": "Fri", "6": "Sat", "7": "Sun"
+};
+var dayCountMapforMaxWin = {
+    "1": 0, "2": 0, "3": 0, "4": 0,
+    "5": 0, "6": 0, "7": 0
+};
+var dayCountMapforMaxLoss = {
+    "1": 0, "2": 0, "3": 0, "4": 0,
+    "5": 0, "6": 0, "7": 0
+};
 
-//check for win or loss
+//Check for win or loss
 checkWinOrLoss = () => {
     ((Math.floor(Math.random() * 10) % 2) == BET_AMOUNT) ?
         console.log(`Win`) : console.log(`Loss`);
 }
 
-//This mthod for resign for the day if 50% loss or 50% win and re
+//This mthod for resign for the day if 50% loss or 50% win and resignDay
 resignDay = () => {
     let cash = stake;
     while (cash < fiftyPercentageWin && cash > fiftyPercentageLoss) {
@@ -22,6 +36,7 @@ resignDay = () => {
     }
     return cash;
 }
+
 //Print record month wise
 printMonthlyRecord = (monthName, totalLostAmount, totalWonAmount, numberOfLostDays, numberOfWonDays) => {
     console.log(`\nmonth       :  ${monthName}`);
@@ -29,30 +44,73 @@ printMonthlyRecord = (monthName, totalLostAmount, totalWonAmount, numberOfLostDa
     console.log(`Won ammount :  ${totalWonAmount}`);
     console.log(`lost days   :  ${numberOfLostDays}`);
     console.log(`won days    :  ${numberOfWonDays}`);
-};
+}
+
+//Find Luckiest day and return it 
+findLuckiestDay = () => {
+    var maxWinValue = 0;
+    var winKey = 1;
+
+    for (let index = 1; index < Object.keys(dayCountMapforMaxWin).length; index++) {
+        if (maxWinValue < dayCountMapforMaxWin[index]) {
+            maxWinValue = dayCountMapforMaxWin[index];
+            winKey = index;
+        }
+    }
+
+    return dayNameMap[winKey];
+}
+
+//Find Unluckiest day and return it.
+findUnLuckiestDay = () => {
+    var maxLossValue = 0;
+    var LossKey = 1;
+    for (let index = 0; index < Object.keys(dayCountMapforMaxLoss).length; index++) {
+        if (maxLossValue < dayCountMapforMaxLoss[index]) {
+            maxLossValue = dayCountMapforMaxLoss[index];
+            LossKey = index;
+        }
+    }
+
+    return dayNameMap[LossKey];
+}
 
 //This method is for play and update win and loss money day wise
 gamblingToatalAmountUpdate = () => {
-    for (var monthName in NumberOfMonthsPerMonth) {
+    for (var monthName in NumberOfDaysPerMonth) {
         var winOrLossMoney = 0;
         var day = 1;
         var numberOfWonDays = 0;
         var numberOfLostDays = 0;
         var totalWonAmount = 0;
         var totalLostAmount = 0;
+        var dayCount = 1;
+        var index = 1;
 
-        while (day <= NumberOfMonthsPerMonth[monthName]) {
+        while (day <= NumberOfDaysPerMonth[monthName]) {
+            if (dayCount > 7) {
+                dayCount = 1;
+                index = 1;
+            }
+
             if (resignDay() == fiftyPercentageWin) {
                 winOrLossMoney = fiftyPercentageWin - stake;
                 totalWonAmount = totalWonAmount + winOrLossMoney;
+
+                dayCountMapforMaxWin[index]++;
                 numberOfWonDays++;
             }
             else {
                 winOrLossMoney = stake - fiftyPercentageLoss;
                 totalLostAmount = totalLostAmount + winOrLossMoney;
+
+                dayCountMapforMaxLoss[index]++;
                 numberOfLostDays++;
             }
+
             day = day + 1;
+            dayCount++;
+            index++;
         }
 
         printMonthlyRecord(monthName, totalLostAmount, totalWonAmount, numberOfLostDays, numberOfWonDays);
@@ -61,3 +119,5 @@ gamblingToatalAmountUpdate = () => {
 
 console.log(`Welcome to Gambling Simulator`);
 gamblingToatalAmountUpdate();
+console.log(`\nLuckiest day     ${findLuckiestDay()}`);
+console.log(`Unluckiest day   ${findUnLuckiestDay()}`);
